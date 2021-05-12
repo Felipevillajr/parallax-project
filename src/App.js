3ecd22dfcd0ecd6c86cf3f11a2b19207bd1d74de
felipevillajr <felipevilla2010@live.com>
2021-05-12 18:56:37 -0400

    added css and base function code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import "./App.scss";
+import "./Parallax.js";
+import React, { useEffect, useState } from "react";
 function App() {
-  return <div></div>;
+  const [mousePositionX, setMousePositionX] = useState({
+    left: 0,
+  });
+
+  const [mousePositionY, setMousePositionY] = useState({
+    left: 0,
+  });
+
+  function handleMouseMove(e) {
+    setMousePositionX({ left: e.pageX });
+    setMousePositionY({ left: e.pageX });
+  }
+
+  function magic(e) {
+    let w = window.innerWidth / 2;
+    let h = window.innerHeight / 2;
+    let mouseX = mousePositionX;
+    let mouseY = mousePositionY;
+    let depth1 = `${50 - (mouseX - w) * 0.01}% 
+    ${50 - (mouseY - h) * 0.01}%`;
+    let depth2 = `${50 - (mouseX - w) * 0.02}% ${50 - (mouseY - h) * 0.02}%`;
+    let depth3 = `${50 - (mouseX - w) * 0.06}%
+    ${50 - (mouseY - h) * 0.06}%`;
+    let x = `${depth3}, ${depth2}, ${depth1}`;
+    console.log(x);
+    e.style.backgroundPosition = x;
+  }
+
+  return (
+    <div onMouseMove={(e) => handleMouseMove(e)} id="parallax">
+      <h1>Effect</h1>
+    </div>
+  );
 }
 
 export default App;
