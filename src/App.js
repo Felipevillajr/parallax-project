e802f99095d43db34a966980bab03be94772cf89
felipevillajr <felipevilla2010@live.com>
2021-05-13 17:25:31 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,44 +1,27 @@
 import "./App.scss";
 import "./Parallax.js";
 import React, { useEffect, useState } from "react";
+import astro from "./Images/astro.png";
+
 function App() {
-  const [mousePositionX, setMousePositionX] = useState({
-    left: 0,
-  });
-
-  const [mousePositionY, setMousePositionY] = useState({
-    top: 0,
-  });
-
   function handleMouseMove(e) {
-    setMousePositionX({ left: e.pageX });
-    setMousePositionY({ top: e.pageX });
-    let w = window.innerWidth / 2;
-    let h = window.innerHeight / 2;
-    let mouseX = mousePositionX.left - w;
-    let mouseY = mousePositionY.top - h;
-    let layer = document.querySelector("#parallax");
-    let bg = document.querySelector(".bg");
+    let w = e.clientX;
+    let h = e.clientY;
 
-    bg.style.transform =
-      "translateX(-" +
-      (0 + mouseX / 1000) +
-      "%) translateY(-" +
-      (0 + mouseY / 1000) +
-      "%)";
+    let layer = document.querySelector("#astro");
+
     layer.style.transform =
-      "translateX(-" +
-      (2 + mouseX / 150) +
-      "%) translateY(-" +
-      (2 + mouseY / 300) +
-      "%)";
+      "translateX(-" + w / 50 + "%) translateY(-" + h / 50 + "%)";
+
+    console.log(e);
   }
-  // useEffect(() => {});
 
   return (
     <>
       <div className="bg"></div>
-      <div onMouseMove={(e) => handleMouseMove(e)} id="parallax"></div>
+      <div onMouseMove={(e) => handleMouseMove(e)} id="parallax">
+        <img id="astro" alt="astronaut" src={astro} />
+      </div>
     </>
   );
 }
